e0ffc038b4f606de2bfe4521894c35a475b74a80
Bushra-B <bushraxbil@gmail.com>
2020-03-16 13:40:33 +0200

    made the code more dynamic

--- js/q2.js
+++ js/q2.js
@@ -1,5 +1,13 @@
 'use strict';
-var starsArray = ['*', '**', '***', '****', '*****'];
-for (var i =0; i < starsArray.length; i++){
-  console.log(starsArray[i]);
+// var starsArray = ['*', '**', '***', '****', '*****'];
+// for (var i =0; i < starsArray.length; i++){
+//   console.log(starsArray[i]);
+// }
+var starsArray = [];
+var stars = '';
+starsArray.push(stars);
+for (var i =0; i < 5; i++){
+  stars += '*';
+  starsArray.push(stars);
+  console.log(starsArray[i+1]);
 }
