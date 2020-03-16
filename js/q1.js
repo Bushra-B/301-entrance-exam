56b6777d4bdd46de22da6a79e3bd21b6118f6bc1
Bushra-B <bushraxbil@gmail.com>
2020-03-16 13:23:48 +0200

    made the code dynamic

--- js/q1.js
+++ js/q1.js
@@ -1,7 +1,18 @@
 'use strict';
-var numbersArray = [4, 2, 3, 1];
+//random numbers generator function:
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+var numbersArray = [];
+//array of random numbers:
+for (var j=0; j < 4; j++){
+  numbersArray.push(getRandomInt(1, 10));
+}
+// console.log(numbersArray);
 var greaterThanArray =[];
-var num = 3;
+var num = getRandomInt(1, 10);
 function greaterThan(){
   for(var i =0; i < numbersArray.length; i++){
     if(numbersArray[i] > num){
@@ -10,5 +21,12 @@ function greaterThan(){
   }
 }
 greaterThan(numbersArray, num);
-console.log(greaterThanArray);
-console.log(greaterThanArray.length);
+// console.log(num);
+// console.log(greaterThanArray);
+// console.log(greaterThanArray.length);
+// console.log(`greaterThan([${numbersArray}], ${num}) => ${greaterThanArray.length}`);
+var solution = document.getElementById('sol');
+var pEl = document.createElement('p');
+pEl.textContent = `greaterThan([${numbersArray}], ${num}) => ${greaterThanArray.length}`;
+solution.appendChild(pEl);
+
